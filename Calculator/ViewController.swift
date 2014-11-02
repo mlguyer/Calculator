3206332d41d11af5dd66b3a61875b6116f346d1d
mlguyer <mlguyer@yahoo.com>
2014-11-01 23:21:05 -0500

    Targeted each button with IBAction

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -20,6 +20,77 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func zero() {
+        println("0")
+    }
+    
+    @IBAction func one() {
+        println("1")
+    }
 
+    @IBAction func two() {
+        println("2")
+    }
+    
+    @IBAction func three() {
+        println("3")
+    }
+    
+    @IBAction func four() {
+        println("4")
+    }
+    
+    @IBAction func five() {
+        println("5")
+    }
+    
+    @IBAction func six() {
+        println("6")
+    }
+    
+    @IBAction func seven() {
+        println("7")
+    }
+    
+    @IBAction func eight() {
+        println("8")
+    }
+    
+    @IBAction func nine() {
+        println("9")
+    }
+    
+    @IBAction func point() {
+        println(".")
+    }
+    
+    @IBAction func equal() {
+        println("=")
+    }
+    
+    @IBAction func plus() {
+        println("+")
+    }
+    
+    @IBAction func minus() {
+        println("-")
+    }
+    
+    @IBAction func multiply() {
+        println("*")
+    }
+    
+    @IBAction func divide() {
+        println("/")
+    }
+    
+    @IBAction func clear() {
+        println("Clear")
+    }
+    
+    @IBAction func clearEntry() {
+        println("Clear Entry")
+    }
+    
 }
 
